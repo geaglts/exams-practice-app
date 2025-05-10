e816cc9112ff7b96a41f18438543977c611517c3
Miguel Angel Toledo Santiago <geaglts@gmail.com>
2025-05-09 20:38:30 -0600

    update tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        "pastel-purple": "#9d9fe6",
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
